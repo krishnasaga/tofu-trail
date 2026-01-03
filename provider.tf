7b4d917867d4ee960cc87ed2bc8bf0c66165b908
krishnasagar <sagar474@hotmail.com>
2026-01-03 14:20:09 +0000

    add remote state config

--- provider.tf
+++ provider.tf
@@ -7,6 +7,13 @@ terraform {
       version = "~> 5.0"
     }
   }
+
+  backend "s3" {
+    bucket  = "my-tofu-state-bucket"
+    key     = "terraform.tfstate"
+    region  = "us-east-1"
+    encrypt = true
+  }
 }
 
 provider "aws" {
--- variables.tf
+++ variables.tf
@@ -7,7 +7,7 @@ variable "aws_region" {
 variable "bucket_name" {
   description = "Name of the S3 bucket"
   type        = string
-  default     = "my-tofu-bucket"
+  default     = "practice-tofu-bucket-1"
 }
 
 variable "environment" {
